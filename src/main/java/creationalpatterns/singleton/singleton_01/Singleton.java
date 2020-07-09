ed3811d63e4f5e30f9c99d9d41dd7f946cde34de
Huangxiaoyuyuyu <253757635@qq.com>
2020-07-10 00:10:56 +0800

    [懒汉式单例模式(非线程安全) 添加为何非线程安全的说明]

--- src/main/java/creationalpatterns/singleton/singleton_01/Singleton.java
+++ src/main/java/creationalpatterns/singleton/singleton_01/Singleton.java
@@ -19,6 +19,20 @@ public class Singleton {
 
     }
 
+    /**
+     * 为何非线程安全：
+     *      由于线程的运行速度是很快的，当第一个线程到达 if(lazySingleton == null)，
+     *      然后开始执行 lazySingleton = new Singleton(); 在lazySingleton还没有被实例化的时候，
+     *      此时第二个线程达到 if(lazySingleton == null),然后也开始执行下面的 lazySingleton = new Singleton();
+     *      最终就出现了两个Singleton对象，这就是所谓的非线程安全。
+     *
+     * 这个现象如何测试：
+     *      通过n个线程来调用getInstance()，从而比对获取到的对象的hashcode是否是一样的。
+     *      如果是线程安全的单例模式，那么不管多少个线程调用getInstance(),最终返回的都是同一个对象，获取的hashcode应该是一样的。
+     *
+     * @return
+     */
+
     //此方法是获得本类实例的唯一全局访问点
     public static Singleton getInstance() {
         //若实例不存在，则new一个新实例，否则返回已有的实例
