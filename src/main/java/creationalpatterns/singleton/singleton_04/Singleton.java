2126a6d778c13091a381cd1c3a34c0f5106b4075
Huangxiaoyuyuyu <253757635@qq.com>
2020-07-12 15:57:28 +0800

    [饿汉式单例模式]

---
+++ src/main/java/creationalpatterns/singleton/singleton_04/Singleton.java
@@ -0,0 +1,26 @@
+package creationalpatterns.singleton.singleton_04;
+
+/**
+ * @Author OliverYu
+ * @Date 2020/7/12 3:41 下午
+ * @Email 253757635@qq.com
+ * @Description 饿汉式单例模式
+ */
+public class Singleton {
+
+    /**
+     * 饿汉式单例模式的优缺点：
+     *      优点：类加载的时候就完成了初始化，避免了线程同步问题。
+     *      缺点：没有延迟加载的效果，如果该类未使用，就会存在内存浪费。
+     */
+    private static Singleton hungrySingleton = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return hungrySingleton;
+    }
+
+}
