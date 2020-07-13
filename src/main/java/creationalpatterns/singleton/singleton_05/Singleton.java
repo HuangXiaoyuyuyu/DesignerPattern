6e30c8edbdfa9f9341355eaef9277ee451eb2585
Huangxiaoyuyuyu <253757635@qq.com>
2020-07-13 23:23:06 +0800

    饿汉式单例模式(实例化过程放在静态代码块中)

---
+++ src/main/java/creationalpatterns/singleton/singleton_05/Singleton.java
@@ -0,0 +1,20 @@
+package creationalpatterns.singleton.singleton_05;
+
+/**
+ * @Author OliverYu
+ * @Date 2020/7/13 11:19 下午
+ * @Email 253757635@qq.com
+ * @Description 饿汉式单例模式(实例化过程放在静态代码块中)
+ */
+public class Singleton {
+
+    private static Singleton hungrySingleton;
+
+    static {
+        hungrySingleton = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return hungrySingleton;
+    }
+}
